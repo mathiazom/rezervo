823c022e6dcde53a40690b7faa1c9836e4c42961
Mathias Oterhals Myklebust <mathias@oterbust.no>
2022-05-07 15:57:39 +0200

    Move cron details to config.yaml

--- cron_generator.py
+++ cron_generator.py
@@ -7,7 +7,7 @@ from consts import APP_ROOT
 STARTUP_DAYS_BEFORE_ACTIVITY = 2
 
 
-def generate_cron_job(index: int, _class: Config, preparation_minutes: int) -> str:
+def generate_cron_job(index: int, _class: Config, cron_config: Config) -> str:
     # Using current datetime simply as a starting point
     # We really only care about the "wall clock" part, which is replaced by input values
     activity_time = datetime.datetime.now().replace(
@@ -18,7 +18,7 @@ def generate_cron_job(index: int, _class: Config, preparation_minutes: int) -> s
     print(f"Activity starts on weekday={_class.weekday} at {activity_time.time()}")
 
     # Back up time to give booking script some prep time
-    cron_time = activity_time - datetime.timedelta(minutes=preparation_minutes)
+    cron_time = activity_time - datetime.timedelta(minutes=cron_config.preparation_minutes)
 
     cron_weekday = (_class.weekday + 1 - STARTUP_DAYS_BEFORE_ACTIVITY) % 7
 
@@ -26,8 +26,8 @@ def generate_cron_job(index: int, _class: Config, preparation_minutes: int) -> s
 
     return (
         f"{cron_time.minute} {cron_time.hour} * * {cron_weekday} "
-        "cd /sit-rezervo/ || exit 1; PATH=$PATH:/usr/local/bin "
-        f"/opt/rikardo/bin/python -u rezervo.py {index} >> /var/log/cron.log 2>&1"
+        f"cd {cron_config.sit_rezervo_dir} || exit 1; PATH=$PATH:/usr/local/bin "
+        f"{cron_config.python_path} -u rezervo.py {index} >> {cron_config.log_path} 2>&1"
         "\n"  # Empty line to please the cron gods ...
     )
 
@@ -39,7 +39,7 @@ def main():
     config = Config.from_config_file(APP_ROOT / "config.yaml")
     cron_spec = ""
     for i, c in enumerate(config.classes):
-        cron_spec += generate_cron_job(i, c, config.booking.preperation_minutes)
+        cron_spec += generate_cron_job(i, c, config.cron)
     with open(sys.argv[1], "w+") as cron_file:
         cron_file.write(cron_spec)
 
