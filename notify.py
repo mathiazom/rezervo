55e1acaf5e67368141dfdc0d34d3ceca53411070
Mathias Oterhals Myklebust <mathias@oterbust.no>
2022-05-07 18:26:36 +0200

    Add .ics upload with transfer.sh

--- notify.py
+++ notify.py
@@ -1,19 +1,39 @@
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 
 import requests
 
 
-def notify_slack(slack_token: str, channel: str, user: str, booked_class: Dict[str, Any], ical_url: str):
+def transfersh_direct_url(transfersh_url: str):
+    url_parts = transfersh_url.split("/")
+    return "/".join(url_parts[:3]) + "/get/" + "/".join(url_parts[3:])
+
+
+def upload_ical_to_transfersh(transfersh_url: str, ical_url: str, filename: str) -> str:
+    return transfersh_direct_url(
+        requests.post(transfersh_url, files={filename: requests.get(ical_url).text}).text
+    )
+
+
+def notify_slack(slack_token: str, channel: str, user: str, booked_class: Dict[str, Any], ical_url: str,
+                 transfersh_url: Optional[str]) -> None:
+    if transfersh_url:
+        filename = f"{booked_class['id']}.ics"
+        print(f"[INFO] Uploading {filename} to {transfersh_url}")
+        ical_tsh_url = upload_ical_to_transfersh(transfersh_url, ical_url, filename)
+        message = f"🤖 <{ical_tsh_url}|*{booked_class['name']}* ({booked_class['from']})> er booket for <@{user}>"
+    else:
+        message = f"🤖 *{booked_class['name']}* ({booked_class['from']}) er booket for <@{user}>"
     print(f"[INFO] Posting booking notification to Slack")
     res = requests.post(
         "https://slack.com/api/chat.postMessage",
         headers={'Authorization': f'Bearer {slack_token}'},
         json={
             "channel": channel,
-            "text": f"🤖 <{ical_url}|*{booked_class['name']}* ({booked_class['from']})> er booket for <@{user}>"
-        })
+            "text": message
+        }
+    )
     if not (res.ok and res.json()['ok']):
         print(f"[FAILED] Could not post booking notification to Slack: {res.text}")
-        return False
+        return
     print(f"[INFO] Booking notification posted successfully to Slack.")
-    return True
+    return
--- rezervo.py
+++ rezervo.py
@@ -133,7 +133,7 @@ def book_class(token, class_id) -> bool:
 
 
 def try_book_class(token: str, _class: Dict[str, Any], max_attempts: int,
-                   slack_config: Optional[Config] = None) -> None:
+                   slack_config: Optional[Config] = None, transfersh_config: Optional[Config] = None) -> None:
     if max_attempts < 1:
         print(f"[WARNING] Max booking attempts should be a positive number")
         return
@@ -151,7 +151,8 @@ def try_book_class(token: str, _class: Dict[str, Any], max_attempts: int,
     print(f"Successfully booked class" + (f" after {attempts} attempts!" if attempts > 1 else "!"))
     if slack_config:
         ical_url = f"{ICAL_URL}/?id={_class['id']}&token={token}"
-        notify_slack(slack_config.bot_token, slack_config.channel_id, slack_config.user_id, _class, ical_url)
+        tsh_url = transfersh_config.url if transfersh_config else None
+        notify_slack(slack_config.bot_token, slack_config.channel_id, slack_config.user_id, _class, ical_url, tsh_url)
     return
 
 
@@ -183,23 +184,24 @@ def main() -> None:
         return
     if _class['bookable']:
         print("Booking is already open, booking now!")
-        try_book_class(auth_token, _class, config.booking.max_attempts, config.slack if "slack" in config else None)
-        return
-    # Retrieve booking opening, and make sure it's timezone aware
-    tz = timezone(config.booking.timezone)
-    opening_time = tz.localize(datetime.fromisoformat(_class['bookingOpensAt']))
-    timedelta = opening_time - datetime.now(tz)
-    wait_time = timedelta.total_seconds()
-    wait_time_string = readable_seconds(wait_time)
-    if wait_time > config.booking.max_waiting_minutes * 60:
-        print(f"[ERROR] Booking waiting time was {wait_time_string}, "
-              f"but max is {config.booking.max_waiting_minutes} minutes. Aborting.")
-        return
-    print(f"Scheduling booking at {datetime.now(tz) + timedelta} "
-          f"(about {wait_time_string} from now)")
-    time.sleep(wait_time)
-    print(f"Awoke at {datetime.now(tz)}")
-    try_book_class(auth_token, _class, config.booking.max_attempts, config.slack if "slack" in config else None)
+    else:
+        # Retrieve booking opening, and make sure it's timezone aware
+        tz = timezone(config.booking.timezone)
+        opening_time = tz.localize(datetime.fromisoformat(_class['bookingOpensAt']))
+        timedelta = opening_time - datetime.now(tz)
+        wait_time = timedelta.total_seconds()
+        wait_time_string = readable_seconds(wait_time)
+        if wait_time > config.booking.max_waiting_minutes * 60:
+            print(f"[ERROR] Booking waiting time was {wait_time_string}, "
+                  f"but max is {config.booking.max_waiting_minutes} minutes. Aborting.")
+            return
+        print(f"Scheduling booking at {datetime.now(tz) + timedelta} "
+              f"(about {wait_time_string} from now)")
+        time.sleep(wait_time)
+        print(f"Awoke at {datetime.now(tz)}")
+    try_book_class(auth_token, _class, config.booking.max_attempts,
+                   config.slack if "slack" in config else None,
+                   config.transfersh if "transfersh" in config else None)
 
 
 if __name__ == '__main__':
