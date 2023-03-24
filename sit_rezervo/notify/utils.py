3527055a4e587603b3d60b7478ba2128734a8f9b
Mathias Oterhals Myklebust <mathias@oterbust.no>
2023-03-24 21:30:10 +0100

    fix: properly construct transfersh direct url

--- sit_rezervo/notify/utils.py
+++ sit_rezervo/notify/utils.py
@@ -1,12 +1,13 @@
+from urllib.parse import urlparse
+
 import requests
 
 
 def upload_ical_to_transfersh(transfersh_url: str, ical_url: str, filename: str) -> str:
-    return transfersh_direct_url(
-        requests.post(transfersh_url, files={filename: requests.get(ical_url).text}).text
-    )
+    return transfersh_direct_url(requests.post(transfersh_url, files={filename: requests.get(ical_url).text}).text)
 
 
-def transfersh_direct_url(transfersh_url: str):
-    url_parts = transfersh_url.split("/")
-    return "/".join(url_parts[:3]) + "/get/" + "/".join(url_parts[3:])
+def transfersh_direct_url(url: str):
+    # prepend '/get' to the url path
+    u = urlparse(url.strip())
+    return u._replace(path=f"/get{u.path}").geturl()
