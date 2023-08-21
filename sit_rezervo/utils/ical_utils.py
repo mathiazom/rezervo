55b72cf4836510adcaf3646d78964be7d55d1d8d
Mathias Oterhals Myklebust <mathias@oterbust.no>
2023-08-21 15:02:22 +0200

    feat: ical sequence numbers

--- sit_rezervo/utils/ical_utils.py
+++ sit_rezervo/utils/ical_utils.py
@@ -17,10 +17,21 @@ def ical_event_from_sit_class_session(session: Session) -> cal.Event:
     event = cal.Event()
     event.add('uid', f'sit-{_class.id}-{session.user_id}@rezervo.no')
     event.add('summary', _class.name)
-    event.add('description', f"{_class.name}{f' med {format_name_list_to_natural([i.name for i in _class.instructors])}' if len(_class.instructors) > 0 else ''}")
+    event.add('description',
+              f"{_class.name}{f' med {format_name_list_to_natural([i.name for i in _class.instructors])}' if len(_class.instructors) > 0 else ''}")
     event.add('location', _class.studio.name)
     event.add('dtstart', datetime.fromisoformat(_class.from_field))
     event.add('dtend', datetime.fromisoformat(_class.to))
     event.add('dtstamp', datetime.now())
     event.add('status', 'CONFIRMED' if session.status in [SessionState.BOOKED, SessionState.CONFIRMED] else 'TENTATIVE')
+    event.add('sequence',
+              # Not strictly compliant, but the status will generally follow this order (except UNKNOWN)
+              # so the sequence number will most likely increase monotonically
+              {
+                  SessionState.UNKNOWN: 0,
+                  SessionState.PLANNED: 1,
+                  SessionState.WAITLIST: 2,
+                  SessionState.BOOKED: 3,
+                  SessionState.CONFIRMED: 4
+              }[session.status])
     return event
