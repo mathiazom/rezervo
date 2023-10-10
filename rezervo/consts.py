723e88c43d36b556983da5ddf13b1f383a16008e
Adrian Andersen <adrianandersen@protonmail.com>
2023-10-11 00:06:11 +0200

    fix(sessions): increase number of weeks to include 3T events in a month

--- rezervo/consts.py
+++ rezervo/consts.py
@@ -1,7 +1,7 @@
 WEEKDAYS = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"]
 
 # The number of whole weeks to fetch in addition to the rest of the current week when looking at planned sessions
-PLANNED_SESSIONS_NEXT_WHOLE_WEEKS = 2
+PLANNED_SESSIONS_NEXT_WHOLE_WEEKS = 4
 
 SLACK_ACTION_ADD_BOOKING_TO_CALENDAR = "add_booking_to_calendar"
 SLACK_ACTION_CANCEL_BOOKING = "cancel_booking"
