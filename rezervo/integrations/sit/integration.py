ec294272d59edc61f4d212866d6b3499cf4bff9d
Adrian Andersen <adrianandersen@protonmail.com>
2023-10-14 21:28:29 +0200

    refactor(sit): use iBooking provider

--- rezervo/integrations/sit/integration.py
+++ rezervo/integrations/sit/integration.py
@@ -1,18 +1,14 @@
 from rezervo.integrations.integration import Integration
-from rezervo.providers.ibooking.booking import (
-    book_class,
-    cancel_booking,
-    find_authed_ibooking_class_by_id,
-    find_public_ibooking_class,
-)
-from rezervo.providers.ibooking.schema import rezervo_class_from_ibooking_class
-from rezervo.providers.ibooking.sessions import fetch_ibooking_sessions
+from rezervo.providers.active import get_provider
+from rezervo.schemas.config.user import IntegrationIdentifier, ProviderIdentifier
+
+ibooking_provider = get_provider(ProviderIdentifier.IBOOKING)(IntegrationIdentifier.SIT)
 
 integration = Integration(
-    find_authed_class_by_id=find_authed_ibooking_class_by_id,
-    find_class=find_public_ibooking_class,
-    book_class=book_class,
-    cancel_booking=cancel_booking,
-    fetch_sessions=fetch_ibooking_sessions,
-    rezervo_class_from_class_data=rezervo_class_from_ibooking_class,
+    find_authed_class_by_id=ibooking_provider.find_authed_class_by_id,
+    find_class=ibooking_provider.find_class,
+    book_class=ibooking_provider.book_class,
+    cancel_booking=ibooking_provider.cancel_booking,
+    fetch_sessions=ibooking_provider.fetch_sessions,
+    rezervo_class_from_class_data=ibooking_provider.rezervo_class_from_class_data,
 )
