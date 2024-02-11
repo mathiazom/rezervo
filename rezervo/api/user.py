77a98c2ba0512ecb236f4d2b482ee02737924e9c
Adrian Andersen <adrianandersen@protonmail.com>
2024-02-11 21:42:27 +0100

    feat(update_user): fetch user name with auth0_mgmt

--- rezervo/api/user.py
+++ rezervo/api/user.py
@@ -1,42 +1,39 @@
+from auth0.management import Auth0
 from fastapi import APIRouter, Depends, HTTPException
-from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from starlette import status
 
 from rezervo.api.common import get_db, token_auth_scheme
 from rezervo.auth import auth0
+from rezervo.auth.auth0 import get_auth0_management_client
 from rezervo.database import crud
 from rezervo.settings import Settings, get_settings
 
 router = APIRouter()
 
 
-class User(BaseModel):
-    name: str
-
-
-@router.put("/user", response_model=User)
+@router.put("/user")
 def upsert_user(
-    user: User,
     token=Depends(token_auth_scheme),
     db: Session = Depends(get_db),
     settings: Settings = Depends(get_settings),
+    auth0_mgmt_client: Auth0 = Depends(get_auth0_management_client),
 ):
+    jwt_sub = auth0.sub_from_jwt(
+        token,
+        settings.JWT_DOMAIN,
+        settings.JWT_ALGORITHMS,
+        settings.JWT_AUDIENCE,
+        settings.JWT_ISSUER,
+    )
+    if not isinstance(jwt_sub, str):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    name = auth0_mgmt_client.users.get(jwt_sub)["name"]  # type: ignore
+
     db_user = crud.user_from_token(db, settings, token)
-
     if db_user is None:
-        jwt_sub = auth0.sub_from_jwt(
-            token,
-            settings.JWT_DOMAIN,
-            settings.JWT_ALGORITHMS,
-            settings.JWT_AUDIENCE,
-            settings.JWT_ISSUER,
-        )
-        if not isinstance(jwt_sub, str):
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-        crud.create_user(db, user.name, jwt_sub)
+        crud.create_user(db, name, jwt_sub)
     else:
-        db_user.name = user.name
+        db_user.name = name
         db.commit()
-
-    return user
