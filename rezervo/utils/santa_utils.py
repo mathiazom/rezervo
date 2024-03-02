16e5139b7561ec7a64d207925ad9c016c2a2885c
Mathias Oterhals Myklebust <mathias@oterbust.no>
2024-03-02 12:59:42 +0100

    fix(santa): timezone aware santa time

--- rezervo/utils/santa_utils.py
+++ rezervo/utils/santa_utils.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+import pytz
+
 
 def check_santa_time() -> bool:
-    return datetime.now().month == 12
+    # TODO: clean this
+    return datetime.now(pytz.timezone("Europe/Oslo")).month == 12
