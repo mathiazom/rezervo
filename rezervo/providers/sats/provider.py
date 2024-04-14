mathiazom/rezervo

commit 36fd57c9b8084abfc598d7505a47414ea1c5ed38

fix(Sats): correct next occurence for same day

rezervo/schemas/config/user.py
@@ -3,6 +3,7 @@ import enum
 from typing import Annotated, Literal, Optional, TypeAlias, Union
 from uuid import UUID
 
+import pytz
 from pydantic.fields import Field
 
 from rezervo.schemas.base import OrmBase
@@ -44,9 +45,14 @@ class Class(CamelModel):
     display_name: Optional[str] = None
 
     def calculate_next_occurrence(self) -> datetime.datetime:
-        now = datetime.datetime.now()
+        now = datetime.datetime.now().astimezone(
+            pytz.timezone("Europe/Oslo")
+        )  # TODO: clean this
         days_ahead = self.weekday - now.weekday()
-        if days_ahead <= 0:
+        if days_ahead < 0 or (
+            days_ahead == 0
+            and (self.start_time.hour, self.start_time.minute) < (now.hour, now.minute)
+        ):
             days_ahead += 7
         target_date = now + datetime.timedelta(days=days_ahead)
         return target_date.replace(

commit 0d996c84f8805e297da9ee2cf3ced610a1d5d5b1

fix(Sats): ignore today when calculating next occurrence from class config
regression introduced in 36fd57c9b8084abfc598d7505a47414ea1c5ed38 because of a misunderstanding of what calculate_next_occurrence should return

rezervo/providers/sats/provider.py
@@ -153,7 +153,7 @@ class SatsProvider(Provider[SatsAuthData, SatsLocationIdentifier], ABC):
             )
 
         return await self._search_for_class(
-            _class_config.calculate_next_occurrence(),
+            _class_config.calculate_next_occurrence(include_today=False),
             1,
             [_class_config.location_id],
             comparator_fn,

rezervo/schemas/config/user.py
@@ -44,14 +44,20 @@ class Class(CamelModel):
     start_time: ClassTime  # TODO: make sure time zones are handled...
     display_name: Optional[str] = None
 
-    def calculate_next_occurrence(self) -> datetime.datetime:
+    def calculate_next_occurrence(
+        self, include_today: bool = True
+    ) -> datetime.datetime:
         now = datetime.datetime.now().astimezone(
             pytz.timezone("Europe/Oslo")
         )  # TODO: clean this
         days_ahead = self.weekday - now.weekday()
         if days_ahead < 0 or (
             days_ahead == 0
-            and (self.start_time.hour, self.start_time.minute) < (now.hour, now.minute)
+            and not (
+                include_today
+                and (now.hour, now.minute)
+                < (self.start_time.hour, self.start_time.minute)
+            )
         ):
             days_ahead += 7
         target_date = now + datetime.timedelta(days=days_ahead)